3a62527d18e96d3ae4c6311e4e58899ce3d1c734
rpetersson <rpetersson@gmail.com>
2020-12-16 12:01:45 +0100

    Added some code
    NOROFF delivery.

---
+++ script.js
@@ -0,0 +1,58 @@
+// question 1
+
+var petArray = ["cat","cow","dog"]
+
+// question 2
+
+console.log(petArray[1])
+
+// question 3
+
+petArray.push("sheep")
+
+// question 4
+
+console.log(petArray.length)
+
+// question 5
+
+var catObject = {
+    name: "kitty",
+    colour: "black",
+    age: 10,
+}
+
+// question 6
+
+console.log(catObject["age"])
+
+// question 7
+
+var catArray = [
+    {
+        name: "kitty",
+        colour: "black",
+        age: 10,
+
+    }
+];
+
+
+// question 8
+
+for(var i = 0; i < catArray.length; i++) {
+
+    console.log(catArray[i]);
+
+}
+
+// question 9
+
+
+function logToConsole(animal) {
+    console.log(animal)
+}
+
+// question 10
+
+logToConsole("donkey")
